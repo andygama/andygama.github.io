d81010bbcf360ebff9f2617bcaf25b80544d8913
Andrew Gamaley <andygama@berkeley.edu>
2018-11-29 04:29:00 -0800

    updating some text links

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,5 +8,16 @@ var waypoint = new Waypoint({
   }
 })
 
-$("a").fadeIn(300);
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topButton").style.display = "block";
+    } else {
+        document.getElementById("topButton").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 }); 
